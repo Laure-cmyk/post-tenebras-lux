dde47c4366e35e376330553616b8ebe3a90689a9
Laure-cmyk <laure.mangold@heig-vd.ch>
2025-05-06 09:37:46 +0200

    BE: added comments to api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\StoryApiController;
 use App\Http\Controllers\ChapterApiController;
 use App\Http\Controllers\ChoiceApiController;
 
+// Route::prefix handles the prefix for all routes in the group
+// Route::group handles the grouping of routes
 
 Route::prefix('api/v1/')->group(function () {
   Route::get('/story', [StoryApiController::class, 'show']);
@@ -12,7 +14,9 @@ Route::prefix('api/v1/')->group(function () {
     Route::delete('/story', [StoryApiController::class, 'destroy']);
     Route::post('/story', [StoryApiController::class, 'store']);
 
+    // Route::apiResource handles the resourceful routes for the controller
+    // By adding apiResource, you automatically get the standard RESTful routes
+    // Using the . allows to define parent-child relationships
     Route::apiResource('story.chapters', ChapterApiController::class);
-    
     Route::apiResource('chapters.choices', ChoiceApiController::class);
 });
